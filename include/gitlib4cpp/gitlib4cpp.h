cd80a219df93bc8a16a635ceba1eb4e0f91d2d08
Lars Froehlich <lars.froehlich@desy.de>
2023-06-07 09:09:43 +0200

    Add unified main include file
    Signed-off-by: Lars Froehlich <lars.froehlich@desy.de>

---
+++ include/gitlib4cpp/gitlib4cpp.h
@@ -0,0 +1,33 @@
+/**
+ * \file   gitlib4cpp.h
+ * \author Lars Froehlich, Jannik Woehnert
+ * \date   Created on June 7, 2023
+ * \brief  Main include file for gitlib4cpp.
+ *
+ * \copyright Copyright 2023 Deutsches Elektronen-Synchrotron (DESY), Hamburg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 2.1 of the license, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#ifndef GITLIB4CPP_GITLIB4CPP_H_
+#define GITLIB4CPP_GITLIB4CPP_H_
+
+#include "gitlib4cpp/Error.h"
+
+/// Namespace git contains all functions, classes, and other declarations of gitlib4cpp.
+namespace git { }
+
+#endif
